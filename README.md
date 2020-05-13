2e898e233f004e9288c328fb335445cd256a41d7
Darkria8 <xuan30166@163.com>
2020-05-13 21:07:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cdfsl-benchmark-master
+ 
---
+++ configs.py
@@ -0,0 +1,9 @@
+save_dir                    = './logs'
+
+miniImageNet_path           	= '/home/data1/WYX/cdfsl-benchmark-master/filelists/miniImagenet'
+DTD_path 						= '/ssd/dtd/images/'
+
+ISIC_path = "/home/data1/WYX/datasets/"
+ChestX_path = "/home/data1/WYX/datasets/chestX"
+CropDisease_path = "/home/data1/WYX/datasets"
+EuroSAT_path = "/home/data1/WYX/datasets/2750"
